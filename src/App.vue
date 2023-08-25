549cc7fbd1cb930709ce4a5a70842b2bde268ac0
phananhtuan <tuanpa@mor.com.vn>
2023-08-25 16:07:04 +0700

    config prettier

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="wrapper">
-      <h1 class="text-center">You’ve successfully created a project with</h1>
+      <h1 class="mt-8 text-center text-6xl">You’ve successfully created a project with</h1>
     </div>
   </header>
 </template>
