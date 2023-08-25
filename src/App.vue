c442f98bd8a67db10e460d4dcd0477e9311c42eb
phananhtuan <tuanpa@mor.com.vn>
2023-08-25 15:51:44 +0700

    config Tailwind CSS IntelliSense

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="wrapper">
-      <h1>You’ve successfully created a project with</h1>
+      <h1 class="text-center">You’ve successfully created a project with</h1>
     </div>
   </header>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
