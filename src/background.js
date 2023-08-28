5b8c1b0dc11246bd3fd20f9efe125bfbc24f5548
phananhtuan <tuanpa@mor.com.vn>
2023-08-28 10:29:43 +0700

    simple test with vtest

---
+++ src/background.js
@@ -0,0 +1,9 @@
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
+
+export const multiply = (num1, num2) => num1 * num2
---
+++ tests/unit/playground.test.js
@@ -0,0 +1,24 @@
+import { evenOrOdd, multiply } from '../../src/background'
+
+describe('basic math', () => {
+  it('adds two numbers', () => {
+    expect(1 + 1).toBe(2)
+  })
+  describe('evenOrOdd', () => {
+    describe('when number is even', () => {
+      it('indicates the number is event', () => {
+        expect(evenOrOdd(4)).toBe('Even')
+      })
+    })
+    describe('when number is odd', () => {
+      it('indicates the number is odd', () => {
+        expect(evenOrOdd(3)).toBe('Odd')
+      })
+    })
+  })
+  describe('multiply', () => {
+    it('multiplies two number together', () => {
+      expect(multiply(2, 3)).toBe(6)
+    })
+  })
+})
